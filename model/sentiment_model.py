ff9e717cd34441abb4ddef2538b5eefa82b199b0
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-09-04 21:23:55 +0530

    change the vectorizer to Tf-idf

--- model/sentiment_model.py
+++ model/sentiment_model.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 import joblib
 
 # Load the dataset
-reviews_df = pd.read_csv("./data/IMDB Dataset.csv")
+reviews_df = pd.read_csv("../data/IMDB Dataset.csv")
 
 # change the sentiment column to numbers (possitive = 0 | negative = 1)
 reviews_df["result"] = reviews_df["sentiment"].apply(lambda x: 1 if x == "negative" else 0)
@@ -16,7 +16,7 @@ reviews_df["result"] = reviews_df["sentiment"].apply(lambda x: 1 if x == "negati
 x_train,x_test,y_train,y_test = train_test_split(reviews_df.review,reviews_df.result,test_size=0.35)
 
 pipeline = Pipeline([
-    ("vectorizer",CountVectorizer()),
+    ("vectorizer",TfidfVectorizer()),
     ("classifier",MultinomialNB())
 ])
 
@@ -24,4 +24,4 @@ pipeline.fit(x_train,y_train)
 accuracy = pipeline.score(x_test,y_test)
 print(f"Accuracy = {accuracy}")
 
-joblib.dump(pipeline,"./model/model.pkl")
+joblib.dump(pipeline,"../model/model.pkl")
